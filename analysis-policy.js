059a96f5c1c7eefc116c982d1542c9a3546673d2
Toine71530 <an.vial.av@gmail.com>
2026-09-12 19:21:50 +0200

    Branche la bibliothèque des défaillances connues sans supprimer l'existant

--- analysis-policy.js
+++ analysis-policy.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import './market-policy.js';
 import './puretech-policy.js';
+import './vehicle-issues-policy.js';
 
 // Complément non destructif : conserve toute la chaîne existante et complète
 // les champs structurés que Gemini peut laisser absents lorsque l'information
